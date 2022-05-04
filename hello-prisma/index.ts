12f0810bd545865eee4fc5bd47cabde780388221
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2022-05-04 09:28:22 +0900

    hello-prisma

---
+++ hello-prisma/index.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+      posts: {
+        create: { title: "Hello World" },
+      },
+      profile: {
+        create: { bio: "I like turtles" },
+      },
+    },
+  });
+
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: { published: true },
+  });
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
+  console.dir(allUsers, { depth: null });
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
