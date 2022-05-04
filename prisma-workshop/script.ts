1c9ca3752ed5167166d39657f4a82f788c9f57a6
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2022-05-05 08:03:49 +0900

    lessn 2 / task 9

--- prisma-workshop/script.ts
+++ prisma-workshop/script.ts
@@ -1,16 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const result = await prisma.post.update({
-    where: { id: 1 },
-    data: {
-      author: {
-        connect: {
-          email: "alice@prisma.io",
-        },
-      },
+  const result = await prisma.user.findUnique({
+    where: { email: "alice@prisma.io" },
+    include: {
+      posts: true,
     },
   });
   console.log(result);
